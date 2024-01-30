ae2cc5ca2341d1c4ad3b582efb77c72bc880d49d
MichaelAnkra <ma970121@ohio.edu>
2024-01-30 13:12:38 -0500

    lab 3 done

---
+++ prog1.cc
@@ -0,0 +1,22 @@
+// Don't forget to include the header block
+// more information on keyword const on pages 95 - 97
+// For if else format look at page 77
+//     @file: prog1.cc
+//     @author: Michael Ankrah
+//     @Date: 01/30/2024
+//     @Brief: correction of Program
+//     @Lab Number: Lab3
+
+#include<iostream> 
+#include<cstdlib>
+using namespace std;
+
+int main() {
+const int INT1 = 15;
+const int INT2= 20;
+
+cout <<"The sum of product is  ";
+cout << INT1 * INT2 << endl;
+
+return 0;
+}
---
+++ prog5.cc
@@ -0,0 +1,30 @@
+// @Name: Michael Ankrah
+// @Date: 01/31/2024
+// @lab number: lab3
+
+
+
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+  
+
+
+int main()
+{
+   double ctof;  // equivalent Celsius temperature
+   double ftoc;  // equivalent Fahrenheit temperature.
+ 
+   int fah = 56;  //declare and initialize at the same time - page 48
+   int cel = 20;
+
+   ctof = 1.8* cel + 32;
+   ftoc =  (fah-32) * 0.55556;
+
+   cout << cel << " degrees Celsius in Fahrenheit is " << ctof << endl;
+   cout << fah << " degrees Fahrenheit in Celsius is " << ftoc << endl;
+   cout << fixed << setprecision(1) ;
+   return (EXIT_SUCCESS);
+}
